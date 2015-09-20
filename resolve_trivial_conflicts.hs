e534c893084cdeaa7438ac60357fdf26770b8ed3
Eyal Lotem <eyal.lotem@gmail.com>
2015-09-20 12:49:13 +0300

    Use consistent 4-space indent style

--- resolve_trivial_conflicts.hs
+++ resolve_trivial_conflicts.hs
@@ -25,34 +25,34 @@ import           System.Process (callProcess, readProcess, readProcessWithExitCo
 import           Prelude.Compat
 
 data Side = A | B
-  deriving (Eq, Ord, Show)
+    deriving (Eq, Ord, Show)
 type LineNo = Int
 
 data Conflict = Conflict
-  { cMarkerA    :: (LineNo, String) -- <<<<<<<....
-  , cMarkerBase :: (LineNo, String) -- |||||||....
-  , cMarkerB    :: (LineNo, String) -- =======....
-  , cMarkerEnd  :: (LineNo, String) -- >>>>>>>....
-  , cLinesA     :: [String]
-  , cLinesBase  :: [String]
-  , cLinesB     :: [String]
-  } deriving (Show)
+    { cMarkerA    :: (LineNo, String) -- <<<<<<<....
+    , cMarkerBase :: (LineNo, String) -- |||||||....
+    , cMarkerB    :: (LineNo, String) -- =======....
+    , cMarkerEnd  :: (LineNo, String) -- >>>>>>>....
+    , cLinesA     :: [String]
+    , cLinesBase  :: [String]
+    , cLinesB     :: [String]
+    } deriving (Show)
 
 prettyConflict :: Conflict -> String
 prettyConflict Conflict {..} =
-  unlines $ concat
-  [ snd cMarkerA    : cLinesA
-  , snd cMarkerBase : cLinesBase
-  , snd cMarkerB    : cLinesB
-  , [snd cMarkerEnd]
-  ]
+    unlines $ concat
+    [ snd cMarkerA    : cLinesA
+    , snd cMarkerBase : cLinesBase
+    , snd cMarkerB    : cLinesB
+    , [snd cMarkerEnd]
+    ]
 
 resolveConflict :: Conflict -> Maybe String
 resolveConflict Conflict{..}
-  | cLinesA == cLinesBase = Just $ unlines cLinesB
-  | cLinesB == cLinesBase = Just $ unlines cLinesA
-  | cLinesA == cLinesB = Just $ unlines cLinesA
-  | otherwise = Nothing
+    | cLinesA == cLinesBase = Just $ unlines cLinesB
+    | cLinesB == cLinesBase = Just $ unlines cLinesA
+    | cLinesA == cLinesB = Just $ unlines cLinesA
+    | otherwise = Nothing
 
 -- '>' -> ">>>>>>> "
 markerPrefix :: Char -> String
@@ -66,66 +66,64 @@ breakUpToMarker c = state (break ((markerPrefix c `isPrefixOf`) . snd))
 
 readHead :: MonadState [a] m => m (Maybe a)
 readHead = state f
-  where
-    f [] = (Nothing, [])
-    f (l:ls) = (Just l, ls)
+    where
+        f [] = (Nothing, [])
+        f (l:ls) = (Just l, ls)
 
 tryReadUpToMarker :: MonadState [(LineNo, String)] m => Char -> m ([(LineNo, String)], Maybe (LineNo, String))
 tryReadUpToMarker c =
-  do
-    ls <- breakUpToMarker c
-    mHead <- readHead
-    return (ls, mHead)
+    do
+        ls <- breakUpToMarker c
+        mHead <- readHead
+        return (ls, mHead)
 
 readUpToMarker :: MonadState [(LineNo, String)] m => Char -> m ([(LineNo, String)], (LineNo, String))
 readUpToMarker c = do
-  res <- tryReadUpToMarker c
-  case res of
-    (ls, Just h)  -> return (ls, h)
-    (ls, Nothing) ->
-      error $ concat
-      [ "Parse error: failed reading up to marker: "
-      , show c, ", got:"
-      , concatMap (\(l,s) -> "\n" ++ show l ++ "\t" ++ s) $ take 5 ls
-      ]
+    res <- tryReadUpToMarker c
+    case res of
+        (ls, Just h)  -> return (ls, h)
+        (ls, Nothing) ->
+            error $ concat
+            [ "Parse error: failed reading up to marker: "
+            , show c, ", got:"
+            , concatMap (\(l,s) -> "\n" ++ show l ++ "\t" ++ s) $ take 5 ls
+            ]
 
 parseConflict :: MonadState [(LineNo, String)] m => (LineNo, String) -> m Conflict
-parseConflict markerA = do
-  (linesA   , markerBase) <- readUpToMarker '|'
-  (linesBase, markerB)    <- readUpToMarker '='
-  (linesB   , markerEnd)  <- readUpToMarker '>'
-  return Conflict
-    { cMarkerA    = markerA
-    , cMarkerBase = markerBase
-    , cMarkerB    = markerB
-    , cMarkerEnd  = markerEnd
-    , cLinesA     = map snd linesA
-    , cLinesB     = map snd linesB
-    , cLinesBase  = map snd linesBase
-    }
+parseConflict markerA =
+    do  (linesA   , markerBase) <- readUpToMarker '|'
+        (linesBase, markerB)    <- readUpToMarker '='
+        (linesB   , markerEnd)  <- readUpToMarker '>'
+        return Conflict
+            { cMarkerA    = markerA
+            , cMarkerBase = markerBase
+            , cMarkerB    = markerB
+            , cMarkerEnd  = markerEnd
+            , cLinesA     = map snd linesA
+            , cLinesB     = map snd linesB
+            , cLinesBase  = map snd linesBase
+            }
 
 parseConflicts :: String -> [Either String Conflict]
 parseConflicts input =
-  snd $ runWriter $ evalStateT loop (zip [1..] (lines input))
-  where
-    loop =
-      do
-        (ls, mMarkerA) <- tryReadUpToMarker '<'
-        tell $ map (Left . snd) ls
-        case mMarkerA of
-          Nothing -> return ()
-          Just markerA ->
-            do
-              tell . return . Right =<< parseConflict markerA
-              loop
+    snd $ runWriter $ evalStateT loop (zip [1..] (lines input))
+    where
+        loop =
+            do  (ls, mMarkerA) <- tryReadUpToMarker '<'
+                tell $ map (Left . snd) ls
+                case mMarkerA of
+                    Nothing -> return ()
+                    Just markerA ->
+                        do  tell . return . Right =<< parseConflict markerA
+                            loop
 
 type SideDiff = (Side, (LineNo, String), [Diff String])
 
 data NewContent = NewContent
-  { _resolvedSuccessfully :: Int
-  , _failedToResolve :: Int
-  , _newContent :: String
-  }
+    { _resolvedSuccessfully :: Int
+    , _failedToResolve :: Int
+    , _newContent :: String
+    }
 
 getConflictDiffs :: Conflict -> [SideDiff]
 getConflictDiffs Conflict{..} =
@@ -134,96 +132,92 @@ getConflictDiffs Conflict{..} =
 
 resolveContent :: [Either String Conflict] -> NewContent
 resolveContent = asResult . mconcat . map go
-  where
-    asResult (Monoid.Sum successes, Monoid.Sum failures, newContent) =
-      NewContent
-      { _resolvedSuccessfully = successes
-      , _failedToResolve = failures
-      , _newContent = newContent
-      }
-    go (Left line) = (Monoid.Sum 0, Monoid.Sum 0, unlines [line])
-    go (Right conflict) =
-      case resolveConflict conflict of
-      Nothing -> (Monoid.Sum 0, Monoid.Sum 1, prettyConflict conflict)
-      Just trivialLines -> (Monoid.Sum 1, Monoid.Sum 0, trivialLines)
+    where
+        asResult (Monoid.Sum successes, Monoid.Sum failures, newContent) =
+            NewContent
+            { _resolvedSuccessfully = successes
+            , _failedToResolve = failures
+            , _newContent = newContent
+            }
+        go (Left line) = (Monoid.Sum 0, Monoid.Sum 0, unlines [line])
+        go (Right conflict) =
+            case resolveConflict conflict of
+            Nothing -> (Monoid.Sum 0, Monoid.Sum 1, prettyConflict conflict)
+            Just trivialLines -> (Monoid.Sum 1, Monoid.Sum 0, trivialLines)
 
 gitAdd :: FilePath -> IO ()
 gitAdd fileName =
-  callProcess "git" ["add", "--", fileName]
+    callProcess "git" ["add", "--", fileName]
 
 data Options = Options
-  { shouldUseEditor :: Bool
-  , shouldDumpDiffs :: Bool
-  , shouldUseColor :: Maybe ColorEnable
-  , shouldSetConflictStyle :: Bool
-  }
+    { shouldUseEditor :: Bool
+    , shouldDumpDiffs :: Bool
+    , shouldUseColor :: Maybe ColorEnable
+    , shouldSetConflictStyle :: Bool
+    }
 instance Monoid Options where
-  mempty = Options False False Nothing False
-  Options oe0 od0 oc0 os0 `mappend` Options oe1 od1 oc1 os1 =
-    Options
-    (combineBool oe0 oe1 "-e")
-    (combineBool od0 od1 "-d")
-    (combineMaybe oc0 oc1 "-c or -C")
-    (os0 || os1)
-    where
-      err flag = error $ "Multiple " ++ flag ++ " flags used"
-      combineMaybe (Just _) (Just _) flag = err flag
-      combineMaybe Nothing Nothing _ = Nothing
-      combineMaybe (Just x) Nothing _ = Just x
-      combineMaybe Nothing (Just y) _ = Just y
-      combineBool True True flag = err flag
-      combineBool x y _ = x || y
+    mempty = Options False False Nothing False
+    Options oe0 od0 oc0 os0 `mappend` Options oe1 od1 oc1 os1 =
+        Options
+        (combineBool oe0 oe1 "-e")
+        (combineBool od0 od1 "-d")
+        (combineMaybe oc0 oc1 "-c or -C")
+        (os0 || os1)
+        where
+            err flag = error $ "Multiple " ++ flag ++ " flags used"
+            combineMaybe (Just _) (Just _) flag = err flag
+            combineMaybe Nothing Nothing _ = Nothing
+            combineMaybe (Just x) Nothing _ = Just x
+            combineMaybe Nothing (Just y) _ = Just y
+            combineBool True True flag = err flag
+            combineBool x y _ = x || y
 
 getOpts :: [String] -> IO Options
 getOpts = fmap mconcat . mapM parseArg
-  where
-    parseArg "-e" = return mempty { shouldUseEditor = True }
-    parseArg "-d" = return mempty { shouldDumpDiffs = True }
-    parseArg "-c" = return mempty { shouldUseColor = Just EnableColor }
-    parseArg "-C" = return mempty { shouldUseColor = Just DisableColor }
-    parseArg "-s" = return mempty { shouldSetConflictStyle = True }
-    parseArg arg =
-      do  prog <- getProgName
-          putStr $ unlines
-            [ "Usage: " ++ prog ++ " [-e] [-d] [-c] [-C] [-s]"
-            , ""
-            , "-e    Execute $EDITOR for each conflicted file that remains conflicted"
-            , "-d    Dump the left/right diffs from base in each conflict remaining"
-            , "-c    Enable color"
-            , "-C    Disable color"
-            , "-s    Configure git's global merge.conflictstyle to diff3 if needed"
-            ]
-          fail $ "Unknown argument: " ++ show arg
+    where
+        parseArg "-e" = return mempty { shouldUseEditor = True }
+        parseArg "-d" = return mempty { shouldDumpDiffs = True }
+        parseArg "-c" = return mempty { shouldUseColor = Just EnableColor }
+        parseArg "-C" = return mempty { shouldUseColor = Just DisableColor }
+        parseArg "-s" = return mempty { shouldSetConflictStyle = True }
+        parseArg arg =
+            do  prog <- getProgName
+                putStr $ unlines
+                    [ "Usage: " ++ prog ++ " [-e] [-d] [-c] [-C] [-s]"
+                    , ""
+                    , "-e    Execute $EDITOR for each conflicted file that remains conflicted"
+                    , "-d    Dump the left/right diffs from base in each conflict remaining"
+                    , "-c    Enable color"
+                    , "-C    Disable color"
+                    , "-s    Configure git's global merge.conflictstyle to diff3 if needed"
+                    ]
+                fail $ "Unknown argument: " ++ show arg
 
 openEditor :: Options -> FilePath -> IO ()
 openEditor opts path
-  | shouldUseEditor opts =
-    do
-      editor <- getEnv "EDITOR"
-      callProcess editor [path]
-  | otherwise = return ()
+    | shouldUseEditor opts =
+        do  editor <- getEnv "EDITOR"
+            callProcess editor [path]
+    | otherwise = return ()
 
 dumpDiffs :: ColorEnable -> Options -> FilePath -> [SideDiff] -> IO ()
 dumpDiffs colorEnable opts filePath diffs
-  | shouldDumpDiffs opts = mapM_ dumpDiff diffs
-  | otherwise = return ()
-  where
-    dumpDiff (side, (lineNo, marker), diff) =
-      do
-        putStrLn $ concat
-            [filePath, ":", show lineNo, ":Diff", show side, ": ", marker]
-        putStr $ unlines $ map (ppDiff colorEnable) diff
+    | shouldDumpDiffs opts = mapM_ dumpDiff diffs
+    | otherwise = return ()
+    where
+        dumpDiff (side, (lineNo, marker), diff) =
+            do  putStrLn $ concat
+                    [filePath, ":", show lineNo, ":Diff", show side, ": ", marker]
+                putStr $ unlines $ map (ppDiff colorEnable) diff
 
 dumpAndOpenEditor :: ColorEnable -> Options -> FilePath -> [SideDiff] -> IO ()
 dumpAndOpenEditor colorEnable opts path diffs =
-  do
-    dumpDiffs colorEnable opts path diffs
-    openEditor opts path
+    do  dumpDiffs colorEnable opts path diffs
+        openEditor opts path
 
 overwrite :: FilePath -> String -> IO ()
 overwrite fileName newContent =
-    do
-        renameFile fileName bkup
+    do  renameFile fileName bkup
         writeFile fileName newContent
         removeFile bkup
     where
@@ -231,37 +225,34 @@ overwrite fileName newContent =
 
 resolve :: ColorEnable -> Options -> FilePath -> IO ()
 resolve colorEnable opts fileName =
-  do
-    content <- parseConflicts <$> readFile fileName
-    case resolveContent content of
-      NewContent successes failures newContent
-        | successes == 0 &&
-          failures == 0 -> do
-            putStrLn $ fileName ++ ": No conflicts, git-adding"
-            gitAdd fileName
-        | successes == 0 -> do
-            putStrLn $ concat
-              [ fileName, ": Failed to resolve any of the "
-              , show failures, " conflicts" ]
-            doDump
-        | otherwise ->
-          do
-            putStrLn $ concat
-              [ fileName, ": Successfully resolved ", show successes
-              , " conflicts (failed to resolve " ++ show failures ++ " conflicts)"
-              , if failures == 0 then ", git adding" else ""
-              ]
-            overwrite fileName newContent
-            if failures == 0
-              then gitAdd fileName
-              else doDump
-        where
-          doDump =
-            dumpAndOpenEditor colorEnable opts fileName
-                [ cDiff
-                | Right conflict <- parseConflicts newContent
-                , cDiff <- getConflictDiffs conflict
-                ]
+    do  content <- parseConflicts <$> readFile fileName
+        case resolveContent content of
+            NewContent successes failures newContent
+                | successes == 0 && failures == 0 ->
+                  do  putStrLn $ fileName ++ ": No conflicts, git-adding"
+                      gitAdd fileName
+                | successes == 0 ->
+                  do  putStrLn $ concat
+                          [ fileName, ": Failed to resolve any of the "
+                          , show failures, " conflicts" ]
+                      doDump
+                | otherwise ->
+                  do  putStrLn $ concat
+                          [ fileName, ": Successfully resolved ", show successes
+                          , " conflicts (failed to resolve " ++ show failures ++ " conflicts)"
+                          , if failures == 0 then ", git adding" else ""
+                          ]
+                      overwrite fileName newContent
+                      if failures == 0
+                          then gitAdd fileName
+                          else doDump
+                where
+                    doDump =
+                        dumpAndOpenEditor colorEnable opts fileName
+                        [ cDiff
+                        | Right conflict <- parseConflicts newContent
+                        , cDiff <- getConflictDiffs conflict
+                        ]
 
 stripNewline :: String -> String
 stripNewline x
@@ -340,8 +331,7 @@ ensureNewline str = str ++ suffix
 withAllStageFiles ::
     FilePath -> (FilePath -> Maybe FilePath -> Maybe FilePath -> IO b) -> IO b
 withAllStageFiles path action =
-    do
-        let stdin = ""
+    do  let stdin = ""
         [baseTmp, localTmp, remoteTmp] <-
             take 3 . words <$>
             readProcess "git" ["checkout-index", "--stage=all", "--", path] stdin
@@ -351,16 +341,14 @@ withAllStageFiles path action =
             mRemoteTmp = maybePath remoteTmp
         action baseTmp mLocalTmp mRemoteTmp
             `E.finally`
-            do
-                removeFile baseTmp
+            do  removeFile baseTmp
                 traverse_ removeFile mLocalTmp
                 traverse_ removeFile mRemoteTmp
 
 deleteModifyConflictAddMarkers :: FilePath -> IO ()
 deleteModifyConflictAddMarkers path =
     withAllStageFiles path $ \baseTmp mLocalTmp mRemoteTmp ->
-    do
-        baseContent <- readFile baseTmp
+    do  baseContent <- readFile baseTmp
         localContent <- maybe (return "") readFile mLocalTmp
         remoteContent <- maybe (return "") readFile mRemoteTmp
         overwrite path $
