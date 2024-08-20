9eab13a1dfac2830d2816573bbc9a39e6ed5b859
Yair Chuchem <yairchu@gmail.com>
2024-08-20 16:09:53 +0300

    Use lens (unnecessary?)
    Note:
    This uses lens and overloaded labels,
    while we strive to keep the code of git-mediate beginner contributor friendly!
    Is there a nicer way to implement this?

--- src/OptUtils.hs
+++ src/OptUtils.hs
@@ -1,23 +1,27 @@
-{-# LANGUAGE OverloadedRecordDot, LambdaCase, FlexibleContexts #-}
+{-# LANGUAGE LambdaCase, DeriveGeneric, OverloadedLabels, FlexibleContexts #-}
 
 module OptUtils
     ( Parser, parseEnvOptions, envSwitch, envOptional, envOption
     ) where
 
 import           Control.Applicative ((<|>))
-import           Control.Monad ((>=>), guard, unless)
+import           Control.Lens.Operators
+import qualified Control.Lens as Lens
+import           Control.Monad (guard, unless)
 import           Control.Monad.Reader
 import           Control.Monad.State
 import           Data.Foldable (traverse_)
 import           Data.Functor.Compose (Compose(..))
+import           Data.Generics.Labels ()
 import qualified Data.Map as M
-import           Data.Maybe (catMaybes)
 import qualified Data.Set as S
+import           GHC.Generics (Generic)
 import qualified Options.Applicative as O
 import           System.Environment (lookupEnv)
 import           Text.Read.Compat (readMaybe)
 
-type Parser = Compose (ReaderT EnvVarName (State EnvContent)) O.Parser
+type Parser = Compose ParseM O.Parser
+type ParseM = ReaderT EnvVarName (State EnvContent)
 
 type EnvVarName = String
 
@@ -25,14 +29,14 @@ data EnvContent = EnvContent
     { flags :: S.Set String
     , options :: M.Map String String
     , errors :: [String]
-    }
+    } deriving Generic
 
 instance Semigroup EnvContent where
     EnvContent f0 o0 e0 <> EnvContent f1 o1 e1 =
         EnvContent (f0 <> f1) (o0 <> o1)
         ( e0 <> e1
-            <> (("Duplicate flag: " <>) <$> S.toList (S.intersection f0 f1))
-            <> (("Duplicate option: " <>) <$> M.keys (M.intersection o0 o1))
+            <> (("Duplicate flag: " <>) <$> S.intersection f0 f1 ^.. Lens.folded)
+            <> (("Duplicate option: " <>) <$> M.intersection o0 o1 ^.. Lens.itraversed . Lens.asIndex)
         )
 
 instance Monoid EnvContent where
@@ -51,7 +55,7 @@ parseEnvOptions name (Compose parser) =
 
 formatRemainder :: EnvContent -> [String]
 formatRemainder (EnvContent f o e) =
-    (("Unrecognized flag --" <>) <$> S.toList f)
+    (("Unrecognized flag --" <>) <$> f ^.. Lens.folded)
     <> M.foldMapWithKey (\k v -> ["Unrecognized option: --" <> k <> " " <> v]) o
     <> e
 
@@ -78,8 +82,9 @@ envSwitch :: String -> Bool -> String -> Parser Bool
 envSwitch name def desc =
     Compose $
     do
-        otherInEnv <- gets (S.member otherMode . flags)
-        modify (\x -> x{flags = S.delete otherMode x.flags})
+        otherInEnv <-
+            Lens.zoom (#flags . Lens.contains otherMode)
+            (get <* put False)
         let flag
                 | otherInEnv = defaultMode
                 | otherwise = otherMode
@@ -115,19 +120,19 @@ envOptional name valDesc help disableHelp =
             opt <- getCompose (envOption name Nothing (commonMods <> ov))
             pure $
                 O.optional opt
-                <|> f <$> O.switch (O.long ("no-" <> name) <> O.help (disableHelp val <> oh))
-        where
-            f True = Nothing
-            f False = Just val
+                <|> (val <$) . (^? Lens.only False) <$> O.switch (O.long ("no-" <> name) <> O.help (disableHelp val <> oh))
     Nothing -> pure (O.optional (O.option O.auto (O.long name <> commonMods)))
     where
         commonMods = O.metavar valDesc <> O.help help
 
-readOption :: (Read a, MonadState EnvContent m) => String -> m (Maybe a)
-readOption name =
+readOption :: Read a => String -> ParseM (Maybe a)
+readOption name = Lens.zoom (#options . Lens.at name) parseOption
+
+parseOption :: (Read a, MonadState (Maybe String) m) => m (Maybe a)
+parseOption =
     do
-        result <- gets (M.lookup name . options >=> readMaybe)
-        result <$ traverse_ (const (modify (\x -> x{options = M.delete name x.options}))) result
+        result <- gets (>>= readMaybe)
+        result <$ traverse_ (const (put Nothing)) result
 
 -- | An option with a default value which may be initialized by an environment variable.
 --
@@ -136,14 +141,14 @@ envOption :: (Read a, Show a) => String -> Maybe Char -> O.Mod O.OptionFields a 
 envOption name shortName mods =
     Compose $
     traverse readOption ([name] <> maybe [] (pure . pure) shortName)
+    <&> (^.. traverse . Lens._Just)
     >>=
     \case
     [] -> pure (O.option O.auto baseMods)
     [val] -> O.option O.auto . (baseMods <>) <$> envOptionFromEnv val
     _ ->
         O.option O.auto baseMods <$
-        modify (\x -> x{errors = x.errors <> ["Both --" <> name <> " and -" <> maybe [] pure shortName <> " specified"]})
-    . catMaybes
+        (#errors <>= ["Both --" <> name <> " and -" <> shortName ^.. Lens._Just <> " specified"])
     where
         baseMods = O.long name <> foldMap O.short shortName <> mods
 
